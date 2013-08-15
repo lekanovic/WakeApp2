afb02bf178a2e56c64bc59d55cc916b106bd5030
Radovan Lekanovic <lekanovic@gmail.com>
2013-08-15 21:45:08 +0200

    Remove unused imports

--- application/src/com/application/trainsnooze/DataBaseHandler.java
+++ application/src/com/application/trainsnooze/DataBaseHandler.java
@@ -8,7 +8,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.location.Location;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
